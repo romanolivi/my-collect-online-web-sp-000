e405731e06902ccbd61915f01b72e37437db3fe4
Romano Livi <romano_livi@outlook.com>
2020-12-12 20:52:58 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,21 @@
+def my_collect(array)
+  i = 0 
+  collection = []
+  
+  while i < array.length 
+  collection << yield(array[i])
+  i += 1 
+  end
+
+  return collection
+  
+end
+array = ["Tim Jones", "Tommy Shelby", "Oswald Mosley"]
+
+my_collect(array) do |name|
+  name.split(" ").first 
+end
+  
+  
 
 
